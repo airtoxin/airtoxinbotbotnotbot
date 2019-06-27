491daedc3d246b1227a6741a1c2b8c2fc250303b
airtoxin <airtoxin@icloud.com>
2019-06-27 15:49:36 +0900

    Link to twitter

--- App.tsx
+++ App.tsx
@@ -1,13 +1,31 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  Linking
+} from "react-native";
+import tweets from "./data/tweet_texts.json";
 
-export default function App() {
+export default () => {
+  const postTweet = (tweet: string) => {
+    Linking.openURL(`twitter://post?message=${tweet}`).catch(console.log);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <FlatList
+        data={tweets as string[]}
+        renderItem={tweet => (
+          <Button onPress={() => postTweet(tweet.item)} title={tweet.item} />
+        )}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
